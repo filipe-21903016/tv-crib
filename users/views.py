c9489a4798d04b4fb1bb901ce352c06518d02513
filipe-21903016 <a21903016@alunos.ulht.pt>
2021-07-05 01:58:32 +0100

    login updated

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
-def login_view(request,user):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
